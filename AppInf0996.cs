fce244c374d0195dd901e75382516a99723982ca
Leonardo Rener <leonardo-rener@hotmail.com>
2022-11-05 17:22:46 -0300

    first commit

---
+++ AppInf0996.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrevisaoTempo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AppInf0996 : Window
+    {
+        public AppInf0996()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuSuperior_Click(object sender, RoutedEventArgs e)
+        {
+            var addButton = sender as FrameworkElement;
+            if (addButton != null)
+                addButton.ContextMenu.IsOpen = true;
+        }
+
+        private void Sair_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
